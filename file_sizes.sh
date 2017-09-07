ee38b8e0cf3594c0f5264f13b251bfa04561f5ca
Sven Kortekaas <sven@skortekaas.nl>
2017-09-07 09:24:23 +0200

    Testing filesizes

--- file_sizes.sh
+++ file_sizes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find ./public/ -type f -size +300k -exec ls -lh {} \; | awk '{ print $8 ": " $5 }'
+find ./public/ \(-iname \*.jpg -o -iname \*.JPG -o -iname \*.jpeg -o -iname \*.JPEG -o -iname \*.png -o -iname \*.PNG -o -iname \*.gif -o -iname \*.GIF\) -type f -size +300k -exec ls -lh {} \; | awk '{ print "Image " $9 " is too big: " $5 }'
 exit 0
