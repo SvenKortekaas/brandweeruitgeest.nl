2dcbe6ff7e717d2080d51b3e7c2766d4fafc386f
Sven Kortekaas <sven@skortekaas.nl>
2017-09-07 11:14:18 +0200

    Omslachtig zo.. goed?

--- file_sizes.sh
+++ file_sizes.sh
@@ -7,4 +7,15 @@ find ./public/ -name "*.png" -type f -size +300k -exec ls -lh {} \; | awk '{ pri
 find ./public/ -name "*.PNG" -type f -size +300k -exec ls -lh {} \; | awk '{ print "PNG/Image " $9 " is too big: " $5 }'
 find ./public/ -name "*.gif" -type f -size +300k -exec ls -lh {} \; | awk '{ print "gif/Image " $9 " is too big: " $5 }'
 find ./public/ -name "*.GIF" -type f -size +300k -exec ls -lh {} \; | awk '{ print "GIF/Image " $9 " is too big: " $5 }'
+
+# Find a way to do this in one line.....
+find ./public/ -name "*.jpg" -type f -size +300k -exec exit 1 \;
+find ./public/ -name "*.JPG" -type f -size +300k -exec exit 1 \;
+find ./public/ -name "*.jpeg" -type f -size +300k -exec exit 1 \;
+find ./public/ -name "*.JPEG" -type f -size +300k -exec exit 1 \;
+find ./public/ -name "*.png" -type f -size +300k -exec exit 1 \;
+find ./public/ -name "*.PNG" -type f -size +300k -exec exit 1 \;
+find ./public/ -name "*.gif" -type f -size +300k -exec exit 1 \;
+find ./public/ -name "*.GIF" -type f -size +300k -exec exit 1 \;
+
 exit 0
