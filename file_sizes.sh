78d87767e338e100211a6c2e746e5f7a20b08b02
Sven Kortekaas <sven@skortekaas.nl>
2017-09-07 11:05:50 +0200

    test

--- file_sizes.sh
+++ file_sizes.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-EXIT_VALUE=0
-find ./public/ -name "*.jpg" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "jpg/Image " $9 " is too big: " $5 }'
-find ./public/ -name "*.JPG" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "JPG/Image " $9 " is too big: " $5 }'
-find ./public/ -name "*.jpeg" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "jpeg/Image " $9 " is too big: " $5 }'
-find ./public/ -name "*.JPEG" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "JPEG/Image " $9 " is too big: " $5 }'
-find ./public/ -name "*.png" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "png/Image " $9 " is too big: " $5 }'
-find ./public/ -name "*.PNG" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "PNG/Image " $9 " is too big: " $5 }'
-find ./public/ -name "*.gif" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "gif/Image " $9 " is too big: " $5 }'
-find ./public/ -name "*.GIF" -type f -size +300k -exec EXIT_VALUE=1\; -exec ls -lh {} \; | awk '{ print "GIF/Image " $9 " is too big: " $5 }'
-exit $EXIT_VALUE
+find ./public/ -name "*.jpg" -type f -size +300k -exec ls -lh {} \; | awk '{ print "jpg/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+find ./public/ -name "*.JPG" -type f -size +300k -exec ls -lh {} \; | awk '{ print "JPG/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+find ./public/ -name "*.jpeg" -type f -size +300k -exec ls -lh {} \; | awk '{ print "jpeg/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+find ./public/ -name "*.JPEG" -type f -size +300k -exec ls -lh {} \; | awk '{ print "JPEG/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+find ./public/ -name "*.png" -type f -size +300k -exec ls -lh {} \; | awk '{ print "png/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+find ./public/ -name "*.PNG" -type f -size +300k -exec ls -lh {} \; | awk '{ print "PNG/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+find ./public/ -name "*.gif" -type f -size +300k -exec ls -lh {} \; | awk '{ print "gif/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+find ./public/ -name "*.GIF" -type f -size +300k -exec ls -lh {} \; | awk '{ print "GIF/Image " $9 " is too big: " $5 }' \; -exec exit 1\;
+exit 0
