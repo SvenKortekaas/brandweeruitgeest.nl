45428d0c1c0379337c8fd33572ccbe3694654eb1
Sven Kortekaas <github@skortekaas.nl>
2018-05-23 14:11:27 +0200

    Change back to old curl ftp

--- deploy.sh
+++ deploy.sh
@@ -80,9 +80,12 @@ rm over-ons/index.html && mv over-ons/index.min.html over-ons/index.html
 html-minifier --case-sensitive --collapse-whitespace -o nieuws/index.min.html nieuws/index.html
 rm nieuws/index.html && mv nieuws/index.min.html nieuws/index.html
 
-cd ..
+#cd ..
+#
+#echo Starting the upload
+#lftp -c "set ssl:verify-certificate no; set dns:order "inet"; set ftp:use-mdtm off; open ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST:21; mirror --ignore-time --continue --reverse --verbose=3 --parallel=10 public .; quit;"
 
 echo Starting the upload
-lftp -c "set ssl:verify-certificate no; set dns:order "inet"; set ftp:use-mdtm off; open ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST:21; mirror --ignore-time --continue --reverse --verbose=3 --parallel=10 public .; quit;"
+find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD $FTP_HOST/{} \;
 
 exit 0
